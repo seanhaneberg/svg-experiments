602978ab150f00e849608f736f56d33818dcb983
Sean Haneberg <haneberg@gmail.com>
2019-01-28 22:44:26 -0800

    hooked up a slider to curve

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import Slider from 'rc-slider';
 import SVGWave from './SVGWave';
 import './App.css';
+import 'rc-slider/assets/index.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { strokeWidth: 1 };
+  }
+
+  handleChange(val) {
+    this.setState({ strokeWidth: val });
+  }
+
   render() {
+    const svg = <SVGWave strokeWidth={this.state.strokeWidth} />;
     return (
       <div className="App">
-        <SVGWave 
-          strokeWidth="5"
-        />
+        {svg}
+        <div style={{ padding: 50 }}>
+          <Slider onChange={this.handleChange.bind(this)} />
+        </div>
       </div>
     );
   }
