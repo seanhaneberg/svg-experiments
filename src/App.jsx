bc8f69a7908d00848f79594dfc754f0369b53858
Sean Haneberg <sean_haneberg@hotmail.com>
2019-01-29 21:15:35 -0800

    Static sine wave (#2)
    * package-lock.json
    
    * added potential props
    
    * add react-input-slider
    
    * hooked up a slider to curve
    
    * add .vscode/
    
    * rc-slider
    
    * wire up path properties to sliders
    
    * implement period width
    
    * wire up amplitude

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
+import Slider from 'rc-slider';
 import SVGWave from './SVGWave';
 import './App.css';
+import 'rc-slider/assets/index.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { strokeWidth: 1 };
+  }
+
+  handleAmplitudeChange(val) {
+    this.setState({ amplitude: val });
+  }
+
+  handleWidthChange(val) {
+    this.setState({ periodWidth: val });
+  }
+
+  handleStrokeChange(val) {
+    this.setState({ strokeWidth: val });
+  }
+
   render() {
+    const svg =
+      <SVGWave
+        strokeWidth={this.state.strokeWidth}
+        periodWidth={this.state.periodWidth}
+        width={this.state.width}
+        amplitude={this.state.amplitude}
+        />;
     return (
       <div className="App">
-        <SVGWave 
-          strokeWidth="5"
-        />
+        {svg}
+        <div style={{ padding: 50 }}>
+          <br></br>
+          Stroke
+          <Slider onChange={this.handleStrokeChange.bind(this)} />
+          <br></br>
+          Height
+          <Slider onChange={this.handleAmplitudeChange.bind(this)} />
+          <br></br>
+          Width
+          <Slider onChange={this.handleWidthChange.bind(this)} />
+        </div>
       </div>
     );
   }
--- src/SVGWave.jsx
+++ src/SVGWave.jsx
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 
+/**
+ * Properties:
+ *  - number of cycles
+ *  - offset
+ *  - color
+ *  - amplitude
+ *  - cycle-width
+ */
+
 class SVGWave extends Component {
   render() {
+    const height = this.props.height ? this.props.height : 200;
+    const width = this.props.width ? this.props.width : 200;
     const strokeWidth = this.props.strokeWidth ? this.props.strokeWidth : 1;
+    const startX = 0;
+    const startY = height / 2;
+    const amplitude = this.props.amplitude ? this.props.amplitude : 50;
+    const periodWidth = this.props.periodWidth ? this.props.periodWidth : 80;
+    const dx1 = startX;
+    const dy1 = startY + amplitude;
+    const dx = dx1 + periodWidth; // Destination
+    const dy = startY; // Destination
+    const dx2 = dx;
+    const dy2 = dy1;
+    const dProp =
+      `M
+      ${startX} ${startY}
+      C
+      ${dx1} ${dy1}
+      ${dx2} ${dy2}
+      ${dx} ${dy}
+      S
+      ${dx * 2} ${startY + (startY - dy1)}
+      ${dx * 2} ${startY}`;
     return (
-        <svg height="400" width="450">
-          <path
-            d="M 10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
-            stroke="black"
-            strokeWidth={strokeWidth}
-            fill="transparent"
-            />
-        </svg>
+      <svg height={height} width={width}>
+        <path
+          d={dProp}
+          stroke="black"
+          strokeWidth={strokeWidth}
+          fill="transparent"
+        />
+      </svg>
     );
   }
 }
