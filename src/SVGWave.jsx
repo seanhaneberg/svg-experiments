09f59f3c2f80da4030e3100ccc0f3a6563b4d28b
Sean Haneberg <haneberg@gmail.com>
2019-02-01 21:20:39 -0800

    wired up cycles

--- src/SVGWave.jsx
+++ src/SVGWave.jsx
@@ -21,6 +21,7 @@ class SVGWave extends Component {
       strokeWidth: minStroke,
       periodWidth: minPeriodWidth,
       amplitude: minAmplitude,
+      numCycles: this.props.numCycles ? this.props.numCycles : 0,
     };
   }
 
@@ -35,15 +36,19 @@ class SVGWave extends Component {
   handleStrokeChange(val) {
     this.setState({ strokeWidth: val });
   }
+
+  handleCyclesChange(val) {
+    this.setState({ numCycles: val });
+  }
+  
   render() {
-    const padding = 2;
 
     const amplitude = this.state.amplitude;
-    const height = amplitude * 2 + (padding * 2);
+    const height = amplitude * 2;
     const periodWidth = this.state.periodWidth;
     const strokeWidth = this.state.strokeWidth;
-    const width = (periodWidth * 2) + (padding * 2) + (strokeWidth * 2);
-    const startX = 0 + padding + strokeWidth;
+    const width = (periodWidth * (this.state.numCycles + 1));
+    const startX = 0;
     const startY = height / 2;
 
     // 1st control pt
@@ -60,6 +65,21 @@ class SVGWave extends Component {
 
     const metadata = this.state;
 
+    const numCycles = this.state.numCycles;
+
+    let suffix =
+      `S
+      ${dx + periodWidth} ${startY + (startY - dy1)}
+      ${dx + periodWidth} ${startY}`;
+    for (let i = 0; i < numCycles; i++) {
+      let thisY = (i % 2) ? dy1 : startY + (startY - dy1);
+      if (i > 0) {
+        suffix += `S ${dx + ((i + 1) * periodWidth)} ${thisY} ${dx + (periodWidth * (i + 1))} ${startY}`;
+      }
+
+    }
+
+
     const dProp =
       `M
       ${startX} ${startY}
@@ -67,9 +87,7 @@ class SVGWave extends Component {
       ${dx1} ${dy1}
       ${dx2} ${dy2}
       ${dx} ${dy}
-      S
-      ${dx + periodWidth} ${startY + (startY - dy1)}
-      ${dx + periodWidth} ${startY}`;
+      ${suffix}`;
     return (
       // Need to return:
       <div className="container">
@@ -80,19 +98,23 @@ class SVGWave extends Component {
             max={maxStroke}
             onChange={this.handleStrokeChange.bind(this)}
           />
-          <br></br>
           height
           <Slider
             min={minAmplitude}
             max={maxAmplitude}
             onChange={this.handleAmplitudeChange.bind(this)}
           />
-          <br></br>
           width
           <Slider
             min={minPeriodWidth}
             max={maxPeriodWidth}
             onChange={this.handleWidthChange.bind(this)}
+          />
+          cycles
+          <Slider
+            min={1}
+            max={15}
+            onChange={this.handleCyclesChange.bind(this)}
           />
         </div>
         <div className="data-pane">
