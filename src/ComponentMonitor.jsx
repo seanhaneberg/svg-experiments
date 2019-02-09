d278b56d83f71ab6612189be8bc5b9451f5c5584
Sean Haneberg <haneberg@gmail.com>
2019-02-08 18:07:43 -0800

    remove cruft

--- src/ComponentMonitor.jsx
+++ src/ComponentMonitor.jsx
@@ -3,18 +3,6 @@ import MetadataSliders from './MetadataSliders';
 import './ComponentMonitor.css'
 
 class ComponentMonitor extends Component {
-  constructor(props) {
-    super(props);
-
-    const subjectProps = this.props.subject.props;
-    const propKeys = subjectProps ? Object.keys(subjectProps) : [];
-    const initialState = {};
-    propKeys.map((val, index) => {
-      return initialState[index] = val;
-    });
-    this.state = { initialState };
-  }
-
   render() {
     return (
       <div className="container">
